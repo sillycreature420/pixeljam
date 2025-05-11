05fc317dd5fec2edfd2d7840880dee2e06ae6f20
kataphrakt0s <cam.m.muir@gmail.com>
2025-05-11 17:16:18 -0600

    Reorganized resources, added debug tileset

--- World/Levels/Script/Level.gd
+++ World/Levels/Script/Level.gd
@@ -2,13 +2,3 @@ extends Node2D
 
 @export var level_name: String
 @export var level_data: LevelData
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
