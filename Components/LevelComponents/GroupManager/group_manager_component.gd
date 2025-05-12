60b42c8d8669c2562362777e9e23e7908dc904be
kataphrakt0s <cam.m.muir@gmail.com>
2025-05-11 20:59:35 -0600

    added final TODO for group manager component

--- Components/LevelComponents/GroupManager/group_manager_component.gd
+++ Components/LevelComponents/GroupManager/group_manager_component.gd
@@ -4,3 +4,7 @@ extends Node
 
 func _ready() -> void:
 	pass
+	
+#TODO Link assign_path function to button in HUD using currently selected group	
+func assign_path(group: UnitGroup, path: Node2D):
+	pass
