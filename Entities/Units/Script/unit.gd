22daa1bc47820799d34065dfa8eebff1a6d79dc2
sillycreature420 <madelynvarney02@gmail.com>
2025-05-12 09:45:14 +0100

    Fixed bugs I created in unit.gd
    By adding the _init, units weren't initialising properly, causing _ready to not be called and other problems.
    
    Also added a debug script which I'm using on the zombie unit to check values at runtime.

--- Entities/Units/Script/unit.gd
+++ Entities/Units/Script/unit.gd
@@ -5,7 +5,6 @@ class_name Unit
 @export var body_part : BodyPart
 @export var legs_part : BodyPart
 
-
 @export var path: Node2D
 
 @onready var health_component : HealthComponent = $%HealthComponent
@@ -13,20 +12,26 @@ class_name Unit
 
 var current_target_path_node: Node2D
 
-func _init(_head_part : BodyPart, _body_part : BodyPart, _legs_part : BodyPart) -> void:
+func _init(_head_part : BodyPart = null, _body_part : BodyPart = null, _legs_part : BodyPart = null) -> void:
 	
-	head_part = _head_part
-	body_part = _body_part
-	legs_part = _legs_part
-	
-	health_component.health = head_part.health_modifier + body_part.health_modifier + legs_part.health_modifier
-	
+	if _head_part: head_part = _head_part
+	else: push_warning("Initialized without headpart")
+	if _body_part: body_part = _body_part
+	else: push_warning("Initialized without bodypart")
+	if _legs_part: legs_part = _legs_part
+	else: push_warning("Initialized without legspart")
+
+	call_deferred("initialize_components")
 	return
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("debug_spawn"):
 		pathfinding.move_to(current_target_path_node.global_position)
 
+func initialize_components():
+	health_component.health = head_part.health_modifier + body_part.health_modifier + legs_part.health_modifier
+	return
 
 func _ready():
 	current_target_path_node = path.get_child(0)
+	pass
---
+++ Scripts/DebugHelper/debug_list.gd
@@ -0,0 +1,30 @@
+extends RichTextLabel
+class_name DebugLabel
+##Put the node you wish to display the variables of into the key, then in the value, create and array
+##and type in the variables you wish to display.
+##This will update every frame at runtime.
+
+@export var show_debug : bool = true
+@export var list_of_nodes : Dictionary[Node,Array]
+
+
+func _ready() -> void:
+	if !show_debug:
+		queue_free()
+
+func _process(_delta : float) -> void:
+	update_text()
+	return
+
+func update_text():
+	text = ""
+	
+	for index in list_of_nodes:
+		var property_array = list_of_nodes[index]
+		
+		add_text(index.name + "\n")
+		for property in property_array:
+			assert(property is String, "The value " + str(property) + " in the array must be a string to display the variable.")
+			add_text(property +  ": " + str(index[property]) + ("\n"))
+		add_text("\n")
+	return
