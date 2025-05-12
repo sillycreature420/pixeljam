9b49596f6b6f349ab06b59c793023c96a0589f00
kataphrakt0s <cam.m.muir@gmail.com>
2025-05-12 13:24:26 -0600

    created TODO for obstacle.gd

--- Entities/Obstacles/Script/obstacle.gd
+++ Entities/Obstacles/Script/obstacle.gd
@@ -1,1 +1,5 @@
 extends Node2D
+
+#TODO Create obstacle script
+# Be able to take damage from an attacking unit as long as that unit is nearby
+# Add points to the Level.current_points when destroyed
