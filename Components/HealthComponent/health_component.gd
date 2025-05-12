dd570a38fd521aa344365fbb49da1f3d1fc22a91
sillycreature420 <madelynvarney02@gmail.com>
2025-05-12 08:36:17 +0100

    Added BodyPart system and updated file structure.
    In the unit script, added variables for the head, body and leg parts which can be set by the _init function.
    
    Added 3 folders to Data/BodyPart corresponding to the 3 types of body parts, HeadParts, BodyParts, LegParts
    
    Added a simple health component onto the unit scene.

---
+++ Components/HealthComponent/health_component.gd
@@ -0,0 +1,38 @@
+extends Node2D
+class_name HealthComponent
+
+var max_health
+var health
+
+signal health_below_zero
+signal full_health_reached
+signal damage_taken(amount_damage_taken : float)
+signal heal_given(amount_heal_given : float)
+
+func _ready() -> void:
+	set_health_to_max_health()
+	return
+
+func set_max_health(new_max_health : float):
+	max_health = new_max_health
+	return
+
+func set_health_to_max_health():
+	health = max_health
+	return
+
+func damage(amount_damage_taken : float):
+	
+	health -= amount_damage_taken
+	if health < 0: health_below_zero.emit(); #TODO decide what happens when health hits 0
+	
+	damage_taken.emit(amount_damage_taken)
+	return
+
+func heal(amount_heal_given : float):
+	
+	health += amount_heal_given
+	if health > max_health: health = max_health
+	
+	heal_given.emit(amount_heal_given)
+	return
--- Entities/Units/Script/unit.gd
+++ Entities/Units/Script/unit.gd
@@ -1,10 +1,27 @@
 extends Node2D
+class_name Unit
+
+@export var head_part : BodyPart
+@export var body_part : BodyPart
+@export var legs_part : BodyPart
+
 
 @export var path: Node2D
 
+@onready var health_component : HealthComponent = $%HealthComponent
 @onready var pathfinding: PathfindingComponent = $PathfindingComponent
 
 var current_target_path_node: Node2D
+
+func _init(_head_part : BodyPart, _body_part : BodyPart, _legs_part : BodyPart) -> void:
+	
+	head_part = _head_part
+	body_part = _body_part
+	legs_part = _legs_part
+	
+	health_component.health = head_part.health_modifier + body_part.health_modifier + legs_part.health_modifier
+	
+	return
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("debug_spawn"):
