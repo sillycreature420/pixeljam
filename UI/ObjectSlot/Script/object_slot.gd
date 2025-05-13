5ecaf1263331158d76f47725d1a18172e984c845
sillycreature420 <madelynvarney02@gmail.com>
2025-05-13 10:40:58 +0100

    Fixed object slot not clearing
    The object slot wasn't clearing it's data when the object was removed from the slot, now it does, yay.

--- UI/ObjectSlot/Script/object_slot.gd
+++ UI/ObjectSlot/Script/object_slot.gd
@@ -14,11 +14,14 @@ func snap_object_to_self():
 #this could be replaced or made more reusable by changing the BodyPartObject to something more general
 func store_object_info(new_object : BodyPartObject):
 	object_held = new_object
-	print(object_held.name)
+	
+	object_held.pickupable_component.picked_up.connect(clear_object_info)
+	
 	snap_object_to_self()
 	return
 
 
 func clear_object_info():
+	object_held.pickupable_component.picked_up.disconnect(clear_object_info)
 	object_held = null
 	return
