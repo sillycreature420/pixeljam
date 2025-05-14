078e6647e89432818cf6c858ad62460c179a635a
kataphrakt0s <cam.m.muir@gmail.com>
2025-05-14 14:00:25 -0600

    created assign_unit_data function in GroupManager

--- Resources/Types/UnitGroup/UnitGroup.gd
+++ Resources/Types/UnitGroup/UnitGroup.gd
@@ -1,6 +1,7 @@
 class_name UnitGroup extends Resource
 
 @export var unit_scene: PackedScene
+@export var unit_data: UnitData
 @export var unit_count: int = 1
 @export var level: int = 1
 var target_path: Node2D
--- Scripts/Autoloads/GroupManager/GroupManager.gd
+++ Scripts/Autoloads/GroupManager/GroupManager.gd
@@ -13,6 +13,11 @@ func _ready() -> void:
 func assign_path(group: UnitGroup, path: Node2D):
 	group.target_path = path
 
+
+func assign_unit_data(group: UnitGroup, unit_data: UnitData):
+	group.unit_data = unit_data
+
+
 func _new_group_added(group: UnitGroup, type: String):
 	var type_lower = type.to_lower()
 	group.unit_scene = load("res://Entities/Units/" + type + "Unit/" + type_lower + "_unit.tscn")
