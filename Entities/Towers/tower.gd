2c0cc13db96b8bc79079dc753038dd62e5095f13
kataphrakt0s <cam.m.muir@gmail.com>
2025-05-15 17:09:18 -0600

    added projectiles to tower attacks

--- Entities/Towers/tower.gd
+++ Entities/Towers/tower.gd
@@ -2,23 +2,24 @@ extends Node2D
 
 # Configuration - damage dealt per attack
 @export var damage: float
+@export var projectile_scene: PackedScene
 
 # State machine and cooldown timer references
 @onready var state_chart: StateChart = $StateChart
 @onready var attack_cooldown: Timer = $AttackCooldown
+@onready var projectiles: Node2D = $Projectiles
 
 # Track units in attack range and the current target
 var units_in_range: Array[Unit]  # List of units within detection range
 var targeted_unit: Unit  # Currently focused unit for attacking
-
-#TODO: Create projectiles that shoot towards units when a tower attacks
+var target_last_position: Vector2
 
 func _ready() -> void:
 	# Initialize state machine properties
 	state_chart.set_expression_property("unit_in_range", false)
 
 
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	# Unit detection and tracking logic
 	
 	# Check all units in the "units" group for proximity
@@ -38,12 +39,22 @@ func _physics_process(_delta: float) -> void:
 	# Clean up units that moved out of range
 	for i in units_in_range.size():
 		var unit = units_in_range[i-1]
-		if unit.global_position.distance_to(global_position) > 64:
+		if unit:
+			if unit.global_position.distance_to(global_position) > 64:
+				units_in_range.remove_at(i)
+		else:
 			units_in_range.remove_at(i)
-
 	# If no units left in range, update state
 	if units_in_range.size() == 0:
 		state_chart.set_expression_property("unit_in_range", false)
+	
+	if targeted_unit:
+		target_last_position = targeted_unit.global_position
+		for projectile in projectiles.get_children():
+			projectile.update_position(targeted_unit.global_position, delta)
+	else:
+		for projectile in projectiles.get_children():
+			projectile.update_position(target_last_position, delta)
 
 
 func _on_attacking_state_processing(_delta: float) -> void:
@@ -60,7 +71,13 @@ func _on_attack_cooldown_timeout() -> void:
 	
 	if targeted_unit:
 		# Deal damage to the targeted unit
-		targeted_unit.health_component.damage(damage)
+		#targeted_unit.health_component.damage(damage)
+		# Fire a projectile
+		var new_projectile = projectile_scene.instantiate()
+		new_projectile.damage = damage
+		new_projectile.target_unit = targeted_unit
+		projectiles.add_child(new_projectile)
+		
 	else:
 		# No valid target - return to idle state
 		state_chart.set_expression_property("unit_in_range", false)
