b083f7c84a13339efa4dcf70a98293e617abb494
kataphrakt0s <cam.m.muir@gmail.com>
2025-05-17 14:27:33 -0600

    added grass to level

--- Entities/Obstacles/Script/obstacle.gd
+++ Entities/Obstacles/Script/obstacle.gd
@@ -11,6 +11,8 @@ func _ready() -> void:
 	health_component.health_below_zero.connect(_on_destroyed)
 	#print(health_component)
 
+
+#TODO: When health reaches a certain threshold, change to the broken gate sprite
 func _on_damage_taken(_amount_damage_taken):
 	pass
 
