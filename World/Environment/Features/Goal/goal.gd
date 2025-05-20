01c0bd16879465972dfc3a2967aa865350bf5271
kataphrakt0s <cam.m.muir@gmail.com>
2025-05-20 05:18:30 -0600

    fixed pathfinding on path2 and added goal hp bar

--- World/Environment/Features/Goal/goal.gd
+++ World/Environment/Features/Goal/goal.gd
@@ -7,7 +7,9 @@ const WIN_MUSIC = preload("res://Assets/Audio/Music/pixeljam level music demo 2.
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	health_component.health_below_zero.connect(_on_destroyed)
-
+	$ProgressBar.min_value = 0
+	$ProgressBar.max_value = health_component.max_health
+	$ProgressBar.value = health_component.current_health
 
 #TODO: Implement level completed logic
 func _on_destroyed():
@@ -20,3 +22,7 @@ func unit_reached_goal(damage_taken: float):
 	EventBus.emit_points_added(1000)
 	%GoalReached.play()
 	health_component.damage(damage_taken)
+
+
+func _on_health_component_damage_taken(amount_damage_taken: float) -> void:
+	$ProgressBar.value = health_component.current_health
